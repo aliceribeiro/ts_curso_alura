69141dafa87d158dc5a7ef44e8214c46697c78a8
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-08-22 21:48:15 -0300

    feat: :sparkles: implementa tempo de execucao com decorator (requisito nao funcional)

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
   protected elemento: HTMLElement;
@@ -17,6 +19,7 @@ export abstract class View<T> {
     }
   } 
   
+  @logarTempoDeExecucao()
   public update(model: T): void {
     let template = this.template(model);
     if (this.escapar) {
