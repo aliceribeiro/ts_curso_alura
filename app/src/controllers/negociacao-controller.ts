f081658ff1c29f0d8aa65b906515fe769f4cdb34
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-08-28 20:23:55 -0300

    feat: :sparkles: cria decorator de propriedade

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,5 @@
+import { domInjector } from '../decorators/dom-injector.js';
+import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
@@ -6,20 +8,21 @@ import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
-  private inputData: HTMLInputElement;
-  private inputQuantidade: HTMLInputElement;
-  private inputValor: HTMLInputElement;
-  private negociacoes = new Negociacoes();
-  private negociacoesView = new NegociacoesView('#negociacoesView');
-  private mensagemView = new MensagemView('#mensagemView');
+    @domInjector('#data')
+    private inputData: HTMLInputElement;
+    @domInjector('#quantidade')
+    private inputQuantidade: HTMLInputElement;
+    @domInjector('#valor')
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
 
-  constructor() {
-    this.inputData = document.querySelector('#data') as HTMLInputElement;
-    this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
-    this.inputValor = document.querySelector('#valor') as HTMLInputElement;
-    this.negociacoesView.update(this.negociacoes); 
+    constructor() {
+      this.negociacoesView.update(this.negociacoes);
   }
 
+  @inspect
   @logarTempoDeExecucao()
   public adiciona(): void {
     const negociacao = Negociacao.criaDe(
