b0d3f65e67faf3cc0bf02ab8cf6029630972c3c0
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-08-28 22:45:37 -0300

    feat: :sparkles: implementa a funcao utilitaria de imprimir

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -4,18 +4,25 @@ export class Negociacao {
         this.quantidade = quantidade;
         this.valor = valor;
     }
-    get data() {
-        const data = new Date(this._data.getTime());
-        return data;
-    }
-    get valume() {
-        return this.quantidade * this.valor;
-    }
     static criaDe(dataString, quantidadeString, valorString) {
         const exp = /-/g;
         const date = new Date(dataString.replace(exp, ','));
         const quantidade = parseInt(quantidadeString);
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+    paraTexto() {
+        return `
+      Data: ${this.data},
+      Quantidade: ${this.quantidade},
+      Valor: ${this.valor}
+    `;
     }
 }
--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,10 +1,21 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao implements Imprimivel {
 
   constructor(
     private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
   ) {}
+  
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+      const exp = /-/g;
+      const date = new Date(dataString.replace(exp, ','));
+      const quantidade = parseInt(quantidadeString);
+      const valor = parseFloat(valorString);
+  
+      return new Negociacao(date, quantidade, valor);
+    }
 
   get data(): Date {
     const data = new Date(this._data.getTime());
@@ -15,12 +26,11 @@ export class Negociacao {
     return this.quantidade * this.valor;
   }
 
-  public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-    const exp = /-/g;
-    const date = new Date(dataString.replace(exp, ','));
-    const quantidade = parseInt(quantidadeString);
-    const valor = parseFloat(valorString);
-
-    return new Negociacao(date, quantidade, valor);
+  public paraTexto(): string {
+    return `
+      Data: ${this.data},
+      Quantidade: ${this.quantidade},
+      Valor: ${this.valor}
+    `;
   }
 }
